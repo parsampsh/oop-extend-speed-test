a64d351ee1cac3b240a1cd0aa591fe331ba0d8ae
parsa shahmaleki <parsampsh@gmail.com>
2021-08-03 17:42:06 +0430

    improve the script

--- generate-code.py
+++ generate-code.py
@@ -10,43 +10,47 @@ class_defs = {
     'python': lambda i: 'class c' + str(i) + '(c' + str(i-1) + '): pass',
 }
 
-# show the help
-if '--help' in sys.argv:
-    print('Usage: python generate-code.py <lang> <count>')
-    print('Example: python generate-code.py php 50000')
-    print('To see list of available languages use --langs option')
-    sys.exit()
+def main(args):
+    """ The main cli """
+    # show the help
+    if '--help' in args:
+        print('Usage: python generate-code.py <lang> <count>')
+        print('Example: python generate-code.py php 50000')
+        print('To see list of available languages use --langs option')
+        sys.exit()
 
-# check for --langs option
-if '--langs' in sys.argv:
-    print('\n'.join(class_defs.keys()))
-    sys.exit()
+    # check for --langs option
+    if '--langs' in args:
+        print('\n'.join(class_defs.keys()))
+        sys.exit()
 
-# get the language name argument
-try:
-    selected_lang = sys.argv[1]
-except:
-    selected_lang = 'php'
+    # get the language name argument
+    try:
+        selected_lang = args[0]
+    except:
+        selected_lang = 'php'
 
-# get the count argument
-try:
-    count = int(sys.argv[2])
-except:
-    count = 10000
+    # get the count argument
+    try:
+        count = int(args[1])
+    except:
+        count = 10000
 
-# search for the language
-try:
-    init_part = langs_init[selected_lang]
-    class_def = class_defs[selected_lang]
-except:
-    print('Language not found')
-    sys.exit(1)
+    # search for the language
+    try:
+        init_part = langs_init[selected_lang]
+        class_def = class_defs[selected_lang]
+    except:
+        print('Language not found')
+        sys.exit(1)
 
-# generate the code
-code = init_part + '\n'
-for i in range(2, count):
-    code += class_def(i) + '\n'
+    # generate the code
+    code = init_part + '\n'
+    for i in range(2, count):
+        code += class_def(i) + '\n'
 
-# return the generated code
-print(code)
+    # return the generated code
+    return code
+
+print(main(sys.argv[1:]))
 
